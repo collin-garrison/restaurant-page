b0cc644e44258be1915d3bae5401210b2fa9eb4b
CollinDG <154558368+CollinDG@users.noreply.github.com>
2025-07-03 16:59:29 -0700

    Add html plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -8,6 +9,12 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            filename: "index.html",
+        }),
+    ],
     module: {
         rules: [
             {
